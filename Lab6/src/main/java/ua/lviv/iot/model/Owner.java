661f026f280198b303a98841f3c88fdc096189d1
yurabozhyk <44144716+yurabozhyk@users.noreply.github.com>
2020-05-21 01:08:59 +0300

    Update Owner.java

--- Lab6/src/main/java/ua/lviv/iot/model/Owner.java
+++ Lab6/src/main/java/ua/lviv/iot/model/Owner.java
@@ -9,7 +9,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 @Entity
-@Table(name = "owner", schema = "antoniuk_db")
+@Table(name = "owner", schema = "bozhyk_db")
 public class Owner {
     private int id;
     private String firstName;
