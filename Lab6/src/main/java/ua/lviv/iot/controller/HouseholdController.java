fd45ad761bac5360f4d0378f773bd04deeef887c
yurabozhyk <44144716+yurabozhyk@users.noreply.github.com>
2020-05-21 01:05:53 +0300

    Update HouseholdController.java

--- Lab6/src/main/java/ua/lviv/iot/controller/HouseholdController.java
+++ Lab6/src/main/java/ua/lviv/iot/controller/HouseholdController.java
@@ -8,7 +8,7 @@ import ua.lviv.iot.service.HouseholdService;
 import java.util.List;
 
 @RestController
-@RequestMapping("/antoniuk_db")
+@RequestMapping("/bozhyk_db")
 public class HouseholdController implements Controller<Household> {
     @Autowired
     HouseholdService householdService;
