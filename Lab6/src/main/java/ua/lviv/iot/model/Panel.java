25470a898ad8ea979756b8d04abb0e2b005aba64
yurabozhyk <44144716+yurabozhyk@users.noreply.github.com>
2020-05-21 01:09:16 +0300

    Update Panel.java

--- Lab6/src/main/java/ua/lviv/iot/model/Panel.java
+++ Lab6/src/main/java/ua/lviv/iot/model/Panel.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
-@Table(name = "panel", schema = "antoniuk_db")
+@Table(name = "panel", schema = "bozhyk_db")
 public class Panel {
     private int id;
     private String type;
